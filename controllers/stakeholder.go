351efb763f35d8d2ca2becc667387283820d178e
rinrey <nofear24ynwa@gmail.com>
2024-03-23 17:24:59 +0700

    fixing stakeholder

--- controllers/stakeholder.go
+++ controllers/stakeholder.go
@@ -13,7 +13,12 @@ func GetStakeholder(c *fiber.Ctx) error {
 	var data []models.Stakeholder
 	database.DB.Find(&data)
 
-	return c.JSON(data)
+	//return c.JSON(data)
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"code":    fiber.StatusOK,
+		"message": "Data ditemukan",
+		"data":    data,
+	})
 }
 
 func ShowStakeholder(c *fiber.Ctx) error {
@@ -23,16 +28,22 @@ func ShowStakeholder(c *fiber.Ctx) error {
 	if err := database.DB.First(&data, id).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return c.Status(http.StatusNotFound).JSON(fiber.Map{
+				"code":    fiber.StatusNotFound,
 				"message": "Data tidak ditemukan",
 			})
 		}
 
 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"code":    fiber.StatusInternalServerError,
 			"message": "Data tidak ditemukan",
 		})
 	}
 
-	return c.JSON(data)
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"code":    fiber.StatusOK,
+		"message": "Data ditemukan",
+		"data":    data,
+	})
 }
 
 func CreateStakeholder(c *fiber.Ctx) error {
@@ -40,53 +51,55 @@ func CreateStakeholder(c *fiber.Ctx) error {
 	var data models.Stakeholder
 	if err := c.BodyParser(&data); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"code":    fiber.StatusBadRequest,
 			"message": err.Error(),
 		})
 	}
 
 	if err := database.DB.Create(&data).Error; err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code":    fiber.StatusInternalServerError,
 			"message": err.Error(),
 		})
 	}
 
-	return c.JSON(data)
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"code":    fiber.StatusOK,
+		"message": "Data berhasil dibuat",
+		"data":    data,
+	})
 }
 
 func UpdateStakeholder(c *fiber.Ctx) error {
 	var data models.Stakeholder
 	if err := c.BodyParser(&data); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"code":    fiber.StatusBadRequest,
 			"message": err.Error(),
 		})
 	}
 
-	if err := database.DB.First(data, data.ID).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return c.Status(http.StatusNotFound).JSON(fiber.Map{
-				"message": "Data tidak ditemukan",
-			})
-		}
+	// if err := database.DB.First(&data, data.ID).Error; err != nil {
+	// 	if err == gorm.ErrRecordNotFound {
+	// 		return c.Status(http.StatusNotFound).JSON(fiber.Map{
+	// 			"message": "Data tidak ditemukan",
+	// 		})
+	// 	}
 
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Data tidak ditemukan",
-		})
-	}
-
-	// if err := database.DB.Save(&data).Error; err != nil {
-	// 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-	// 		"message": err.Error(),
+	// 	return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+	// 		"message": "Data tidak ditemukan",
 	// 	})
 	// }
 
 	if err := database.DB.Updates(&data).Error; err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code":    fiber.StatusInternalServerError,
 			"message": err.Error(),
 		})
 	}
 
-	return c.Status(http.StatusOK).JSON(fiber.Map{
-		"code":    http.StatusOK,
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"code":    fiber.StatusOK,
 		"message": "Data berhasil diperbarui",
 		"data":    data,
 	})
